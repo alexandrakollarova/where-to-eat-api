657fb48b672ededdebab28875838dd712763e9ad
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-06 09:30:02 -0500

    cors set up

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
 const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+const { NODE_ENV, CLIENT_ORIGIN } = require('./config')
 
 const app = express()
 
@@ -13,7 +13,11 @@ const morganOption = (NODE_ENV === 'production')
 
 app.use(morgan(morganOption))
 app.use(helmet())
-app.use(cors())
+app.use(
+  cors({
+      origin: CLIENT_ORIGIN
+  })
+)
 
 app.get('/', (req, res) => {
    res.send('Hello, world!')

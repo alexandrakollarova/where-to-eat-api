1835c5b6fa9c13c3500aa9c05b3a6741e7499d9e
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-07 12:57:13 -0500

    added bussinesses route

---
+++ src/businesses/businesses-router.js
@@ -0,0 +1,29 @@
+// const path = require('path')
+const express = require('express')
+const request = require('request')
+// const xss = require('xss')
+
+const businessesRouter = express.Router()
+
+businessesRouter
+    .route('/')
+    .get((req, res, next) => {
+        request({
+            host: "api.yelp.com",
+            uri: "https://api.yelp.com/v3/businesses/search?term=food&location=boston",
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+              "Authorization": "Bearer X3oRfIMZfywOkzjRrgeCMUUN93rljhumD2Gtx0UBIeFvpvK3fK8XUYfEjjDsUg4rsCyFu2-QOl1NIFNFPEkok92cPwPH-KquWBb6T5Qk-q0N3M7TEXGI6D89J3vDXXYx"
+            }
+        },
+        function(error, res, body) {
+            if (!error) {
+                console.log(body)
+            } else {
+                res.json(error)
+            }
+        })
+    })
+
+module.exports = businessesRouter

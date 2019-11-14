19e990c23eda471b514891caa35235ada1011372
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-13 19:07:44 -0500

    fix: local storage value undefined

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -40,10 +40,10 @@ UsersRouter
               newUser
             )
               .then(user => {
-                res
-                  .status(201)
-                  .location(path.posix.join(req.originalUrl, `/${user.id}`))
-                  .json(UsersService.serializeUser(user))
+                // res
+                //   .status(201)
+                  // .location(path.posix.join(req.originalUrl, `/${user.id}`))
+                  // .json(UsersService.serializeUser(user))
 
                   const sub = user.user_name
                   const payload = { user_id: user.id }

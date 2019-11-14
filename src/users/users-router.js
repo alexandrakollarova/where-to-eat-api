e152a343d8026a8fc4622e24a93747774f4d8e0e
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-14 09:44:32 -0500

    fixed cors error

--- src/users/users-router.js
+++ src/users/users-router.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 const UsersService = require('./users-service')
-const AuthService = require('./auth-service')
 
 const UsersRouter = express.Router()
 

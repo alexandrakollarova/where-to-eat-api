8281e203cb69354ed5c45f3c1bddc8adea7437af
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-11-20 12:07:35 -0500

    switching to deployed server

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,7 +6,8 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-    "ssl": !!process.env.SSL
+    //"ssl": !!process.env.SSL 
+    "ssl": true
   // "host": 'ec2-107-21-201-238.compute-1.amazonaws.com',
   // "port": 5432,
   // "database": 'd3vt7quujogoos',

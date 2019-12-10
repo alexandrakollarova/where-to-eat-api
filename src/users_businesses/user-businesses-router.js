aac5478f976442a3be2588fd3ba6d3b130d9c873
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-12-10 09:53:53 -0500

    working on getting user's saved businesses back to client

--- src/users_businesses/user-businesses-router.js
+++ src/users_businesses/user-businesses-router.js
@@ -2,20 +2,61 @@ const express = require("express");
 const BusinessService = require("./business-service");
 const AuthService = require("../auth/auth-service");
 const UsersBusinessesService = require("./user-businesses-service");
+const fetch = require('node-fetch');
 
 const UsersBusinessesRouter = express.Router();
 
 UsersBusinessesRouter.route("/")
   .get((req, res, next) => {
     const activeUser = req.query.user;
-    
+
     let encodedUser = AuthService.verifyJwt(activeUser);
     let user = { user_id: encodedUser.user_id };
-console.log("here")
-    BusinessService.getUsersBusinesses(
-      req.app.get(db),
-      user
+
+    UsersBusinessesService.getUsersBusinessesId(
+      req.app.get("db"),
+      user.user_id
     )
+      .then(async businesses => {
+        console.log(businesses)
+        // const requests = businesses.map(business => {
+        // return fetch(`https://api.yelp.com/v3/businesses/${business.business_id}`,
+        //   {
+        //     method: "GET",
+        //     headers: {
+        //       "Content-Type": "application/json",
+        //       "Authorization": "Bearer X3oRfIMZfywOkzjRrgeCMUUN93rljhumD2Gtx0UBIeFvpvK3fK8XUYfEjjDsUg4rsCyFu2-QOl1NIFNFPEkok92cPwPH-KquWBb6T5Qk-q0N3M7TEXGI6D89J3vDXXYx",
+        //       "Access-Control-Allow-Origin": "*",
+        //       "Access-Control-Allow-Credentials": true
+        //     }
+        //   });
+        // });
+        //Promise.all(requests).then(data => res.json(data))
+        // Promise.all(requests)
+        //   .then(responses => {
+        //   const toJson = responses.map(response => response.json());
+        //     Promise.all(toJson).then(data => console.log(data));
+        //   })
+        const data = [];
+
+        for (const { business_id } of businesses) {
+          const fromYelp = await fetch(`https://api.yelp.com/v3/businesses/${business_id}`,
+            {
+              method: "GET",
+              headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer X3oRfIMZfywOkzjRrgeCMUUN93rljhumD2Gtx0UBIeFvpvK3fK8XUYfEjjDsUg4rsCyFu2-QOl1NIFNFPEkok92cPwPH-KquWBb6T5Qk-q0N3M7TEXGI6D89J3vDXXYx",
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true
+              }
+            }
+          );
+          const jsonData = await fromYelp.json();
+          data.push(jsonData);
+        }
+        console.log(data)
+        res.json(data);
+      })
   })
 
   .post((req, res, next) => {
@@ -40,9 +81,9 @@ console.log("here")
           return BusinessService.saveBusiness(req.app.get("db"), business);
         }
       })
-      .then(() => { 
+      .then(() => {
         UsersBusinessesService.getBusinessId(req.app.get("db"), business).then(
-          data => { 
+          data => {
             let id = data.id
             console.log(id)
             UsersBusinessesService.saveBusinessWithUser(
@@ -75,23 +116,23 @@ console.log("here")
     let business = { business_id: businessId };
 
     UsersBusinessesService.getBusinessId(req.app.get("db"), business).then(
-      data => { 
+      data => {
         let id = data.id
-        
+
         UsersBusinessesService.deleteBusinessFromUser(
-          req.app.get("db"), 
+          req.app.get("db"),
           id
         )
           .then(business => {
             console.log("NUMBER OF DELETED ROWS", business)
             res.sendStatus(200)
           })
-            .catch(next);
-          });
+          .catch(next);
+      });
 
-    })
+  })
 
 
-   
+
 
 module.exports = UsersBusinessesRouter;
--- src/users_businesses/user-businesses-service.js
+++ src/users_businesses/user-businesses-service.js
@@ -1,9 +1,15 @@
 const UserBusinessesService = {
-	getUsersBusinesses(db, userId) {
+	getUsersBusinessesId(db, userId) {
 		return db 
 			.select('*')
 			.from('user_businesses')
-			.where(userId)
+			.where("user_id", userId)
+			.then(rows => {
+				return db
+					.select('*')
+					.from('business')
+					.whereIn('id', rows.map(row => row.business_id))
+		})
 	},
 
 	saveBusinessWithUser(db, userId, businessId) {

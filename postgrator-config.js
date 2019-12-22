92eb208aaff6fe705c7ef6a41ca1c347455152a6
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-12-22 14:04:07 +0100

    deploying the app as MVP

--- postgrator-config.js
+++ postgrator-config.js
@@ -6,5 +6,9 @@ module.exports = {
   "connectionString": (process.env.NODE_ENV === 'test')
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL,
-    "ssl": true,
+  "ssl": true,
+  "ssl": !!process.env.SSL,
+  "host": 'ec2-107-21-201-238.compute-1.amazonaws.com',
+  "port": 5432,
+  "database": 'd3vt7quujogoos',
 }

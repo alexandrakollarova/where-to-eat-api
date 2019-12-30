90aee14c95f38149bcca97f3d9a1b14641ea5d72
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2019-12-30 14:13:32 -0500

    debugging cors error

--- src/app.js
+++ src/app.js
@@ -16,24 +16,10 @@ const morganOption = (NODE_ENV === 'production')
   ? 'tiny'
   : 'common';
 
-const allowCrossDomain = function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.sendStatus(200);
-  }
-  else {
-    next();
-  }
-};
-
-app.use(allowCrossDomain)
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(jsonBodyParser)
+app.use(cors())
 
 const bodyParser = require('body-parser')
 

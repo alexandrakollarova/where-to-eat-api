ea092caf2d17ecdf1a38f319f17aacba7d192081
Alexandra Kollarova <alexandrakollarova91@gmail.com>
2020-01-03 18:20:14 -0500

    working on readme

--- src/businesses/businesses-router.js
+++ src/businesses/businesses-router.js
@@ -4,22 +4,21 @@ const fetch = require('node-fetch')
 const BusinessesRouter = express.Router()
 
 BusinessesRouter
-    .get('/', (req, res, next) => {
-        let lat = req.query.lat
-        let long = req.query.long
-        
-        fetch(`https://api.yelp.com/v3/businesses/search?term=food&latitude=${lat}&longitude=${long}&limit=50`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer X3oRfIMZfywOkzjRrgeCMUUN93rljhumD2Gtx0UBIeFvpvK3fK8XUYfEjjDsUg4rsCyFu2-QOl1NIFNFPEkok92cPwPH-KquWBb6T5Qk-q0N3M7TEXGI6D89J3vDXXYx",
-            "Access-Control-Allow-Origin" : "*", 
-            "Access-Control-Allow-Credentials" : true 
-            }
-        })
-        .then(res => res.json())
-        .then(data => res.json(data))      
-          
-    })   
+  .get('/', (req, res) => {
+    let lat = req.query.lat
+    let long = req.query.long
+
+    fetch(`https://api.yelp.com/v3/businesses/search?term=food&latitude=${lat}&longitude=${long}&limit=50`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": "Bearer X3oRfIMZfywOkzjRrgeCMUUN93rljhumD2Gtx0UBIeFvpvK3fK8XUYfEjjDsUg4rsCyFu2-QOl1NIFNFPEkok92cPwPH-KquWBb6T5Qk-q0N3M7TEXGI6D89J3vDXXYx",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      }
+    })
+      .then(res => res.json())
+      .then(data => res.json(data))
+  })
 
 module.exports = BusinessesRouter
